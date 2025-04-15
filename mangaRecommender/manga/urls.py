bc8ae126c4035a50c7d03f767a6278f9d53f3609
centSadili <sadiliva@students.nu-moa.edu.ph>
2025-04-15 20:35:25 +0800

    Added Frontend (Reactjs) And Backend (Django) with user register/log api on backend

--- mangaRecommender/manga/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('search/', views.search_manga, name='search_manga'),
-    path('allmanga/', views.get_all_manga, name='get_all_manga'),
-]
