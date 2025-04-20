93799d8b4959534d2de6f8c115ad1db1ba948316
Elijah Gundayao <elijahgundayao10@gmail.com>
2025-04-20 20:58:12 +0800

    Fixed Header

--- frontend-manga-recommender-system/src/components/custom/Header.jsx
+++ frontend-manga-recommender-system/src/components/custom/Header.jsx
@@ -1,30 +1,42 @@
 import React from 'react';
 import '../styles/Header.css';
+import { useNavigate } from "react-router-dom";
+
 
 
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const Gohome= ()=>{
+        navigate('home');
+    }
+
     return (
         <div className="header">
             <nav className="nav">
                 <div className="nav-container">
-                    <a className="nav-logo" href="#">KwenToon</a>
+                    <a className="nav-logo" href="home" onClick={Gohome}>KwenToon</a>
                     <button 
                         className="nav-toggle" 
                         type="button"
+                        
                     >
                         <span className="toggle-icon"></span>
                     </button>
                     <div className="nav-links">
                         <ul className="nav-list">
                             <li className="nav-item">
-                                <a className="nav-link active" href="/home">Home</a>
+                                <a className="nav-link active"  href="home" onClick={Gohome}>Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">About</a>
+                                <a className="nav-link" href="#">Popular</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled">Contact</a>
+                                <a className="nav-link ">Completed</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">MyList</a>
                             </li>
                         </ul>
                     </div>
