393a17974dfda0e271c192d91b2f1cbcbe78029f
centSadili <sadiliva@students.nu-moa.edu.ph>
2025-04-24 17:13:36 +0800

    Added favorite model

--- backend_manga_recommender_system/favorite/models.py
+++ backend_manga_recommender_system/favorite/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Favorite(models.Model):
+    favorite_id = models.AutoField(primary_key=True, )
+    user_id = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name="+")
+    mal_id = models.CharField(max_length=100, default='')
+    date = models.DateTimeField(auto_now_add=True)
+    rating = models.FloatField(default=0)
+    
+
+    def __str__(self):
+        return f"Favorite {self.favorite_id} for User {self.user_id} on Manga {self.mal_id}"
+
